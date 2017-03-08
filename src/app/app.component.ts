425ce89eb5a8c18b4c3e6ae52ad9cefa926c304b
44007238 <hardik1.patel@noexternalmail.hsbc.com>
2017-03-08 17:48:25 +0530

    Demo of ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'Sample Angular Application!';
+  name: string;
+  address: string;
+
+  @Input()
+  public users : Array<User> = [];
+
+  ngOnInit(): void {
+    console.log('test Working');
+    this.users.push({name:'Mubarak', address: 'Address 1'});
+    this.users.push({name:'Amol', address: 'Address 2'});
+  }
+
+  addToList(){
+    this.users.push({name: this.name, address: this.address});
+  }
+}
+
+export interface User{
+  name: string;
+  address: string;
 }
