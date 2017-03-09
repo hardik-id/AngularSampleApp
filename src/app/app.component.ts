ae5c7f51fb3a41a25e3b95ef61411ed83e25d43e
44007238 <hardik1.patel@noexternalmail.hsbc.com>
2017-03-09 14:28:42 +0530

    New Component added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,29 @@ export class AppComponent implements OnInit {
   public title = 'Sample Angular Application!';
 
   @Input()
+  public newUser: User = {};
+
+  @Input()
   public users: Array<User> = [];
 
   ngOnInit(): void {
     console.log('test Working');
-    this.users.push({name:'Sandeep', address: 'Address 1', phone_number: 1234, state :0});
-    this.users.push({name:'Amol', address: 'Address 2', phone_number:232323, state :1});
+    this.users.push({name:'Sandeep', address: 'Address 1', phone_number: 1234});
+    this.users.push({name:'Amol', address: 'Address 2', phone_number:232323});
   }
 
-  alertName(userName){
-    alert('Selected user is '+ userName);
+  addToList(){
+    this.users.push({name:this.newUser.name, address: this.newUser.address, phone_number:this.newUser.phone_number});
+  }
+
+  deleteUser(i){
+    this.users.splice(i, 1);
   }
 }
 
 export interface User {
-  name: string;
-  address: string;
-  phone_number: number;
-  state: number;
+  name?: string;
+  address?: string;
+  phone_number?: number;
+  state?: string;
 }
